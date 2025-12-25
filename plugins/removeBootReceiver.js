ed5146c72d4a1f6b626d315dd16c5a4d4ba6a597
iAmTechified <praiseontheweb@gmail.com>
2025-12-25 19:24:16 +0100

    Towards Completion

--- plugins/removeBootReceiver.js
+++ plugins/removeBootReceiver.js
@@ -1,17 +1,29 @@
 const { withAndroidManifest } = require('@expo/config-plugins');
 
-module.exports = function removeBootReceiver(config) {
+module.exports = function removeBootCompleted(config) {
   return withAndroidManifest(config, (config) => {
     const manifest = config.modResults;
-
     const app = manifest.manifest.application?.[0];
-    if (!app || !app.receiver) return config;
 
-    app.receiver = app.receiver.filter(
-      (r) =>
-        r['$']?.['android:name'] !==
-        'expo.modules.audio.AudioRecordingService$AudioRecordingReceiver'
-    );
+    if (!app) return config;
+
+    // Remove BOOT_COMPLETED from ALL receivers
+    if (app.receiver) {
+      app.receiver = app.receiver.map((receiver) => {
+        if (!receiver['intent-filter']) return receiver;
+
+        receiver['intent-filter'] = receiver['intent-filter'].filter(
+          (intent) =>
+            !intent.action?.some(
+              (a) => a['$']?.['android:name'] === 'android.intent.action.BOOT_COMPLETED'
+            )
+        );
+
+        return receiver;
+      }).filter(
+        (receiver) => receiver['intent-filter']?.length !== 0
+      );
+    }
 
     return config;
   });
